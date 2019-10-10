043571ad47173a62fcff7008025d1f42f13dcb62
SebFra97 <sebastian.krzysztof.franc@gmail.com>
2019-10-10 22:40:13 +0200

    working Lab2

--- src/Lab1/Czestosc.java
+++ src/Lab1/Czestosc.java
@@ -3,6 +3,7 @@ package Lab1;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
 
@@ -15,14 +16,26 @@ public class Czestosc {
             Map<String, Integer> mp= new TreeMap<>();
 
             while (sc.hasNext()) {
-                String word = toUpperCase(sc.next()).replaceAll("[^\\p{L}\\p{N}]+", "");
+                String word = toUpperCase(sc.next()).replaceAll("[^\\p{L}\\p{N}]+", "").replaceAll(",","");
                 if (mp.containsKey(word)) {
                     mp.put(word, mp.get(word) + 1);
                 } else {
                     mp.put(word, 1);
                 }
             }
-            System.out.println(mp);
+            int i = 0;
+
+            for(Map.Entry<String,Integer> ent : mp.entrySet()) {
+                if (i != 0 && i < 11) {
+                    String k = ent.getKey();
+                    Integer v = ent.getValue();
+
+                    System.out.println("Słowo " + k + " wystąpiło : " + v + " raz(y)");
+                    i++;
+                }else if (i == 0) i++;
+
+                else break;
+            }
         }catch (FileNotFoundException e) {
             e.printStackTrace();
         }
