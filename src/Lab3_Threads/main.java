84473809c258e224b5b27fdcf04059432a461555
SebFra97 <sebastian.krzysztof.franc@gmail.com>
2019-10-27 18:23:12 +0100

    few updates

--- src/Lab3_Threads/main.java
+++ src/Lab3_Threads/main.java
@@ -31,10 +31,8 @@ class ThreadTest1 implements Runnable {
 class ThreadTest2 implements Runnable {
     @Override
     public void run() {
-        for (int i = 0; i < 2; i++) {
-            double randomNumber = Math.random();
-            randomNumber = randomNumber * 20 + 100;
-            System.out.println((int) randomNumber);
+        for (int i = 100; i <= 120; i++) {
+            System.out.println(i);
         }
     }
 }
